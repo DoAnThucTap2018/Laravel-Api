5fd7c2d5f985606e67240bb8fc8bcce2a1912878
ishieric <ngocnam071094@gmail.com>
2018-06-07 19:01:20 +0700

    BD

--- database/migrations/2018_06_07_115203_create_slide_table.php
+++ database/migrations/2018_06_07_115203_create_slide_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTable extends Migration
+class CreateSlideTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('slide', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('item_id')->default(0);
-            $table->decimal('price');
+            $table->longText('link')->nullable();
             $table->longText('image')->nullable();
             $table->timestamps();
         });
@@ -29,6 +28,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('slide');
     }
 }
